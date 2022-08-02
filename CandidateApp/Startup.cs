16e5545f97bec523796fe82c89f2450cd2b64c12
Filip Radosavljević <filip6331@gmail.com>
2022-08-02 13:09:30 +0200

    U klasi Startup.cs konfigurisana kontekst klasa

--- CandidateApp/Startup.cs
+++ CandidateApp/Startup.cs
@@ -1,7 +1,9 @@
+using CandidateApp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,9 +18,16 @@ namespace CandidateApp
 {
     public class Startup
     {
+        // Deklarišemo promenjivu u koju smeštamo putanju ka bazi
+        public string ConnectionString { get; set; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            // Dodeljujemo ovoj promenjivost vrednost, odmah pri pozivu ove klase u nekoj drugoj klasi
+            // Ovaj naziv "DefaultConnectionString" treba da bude isti kao naziv baze u fajlu appsettings.json
+            ConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -28,6 +37,11 @@ namespace CandidateApp
         {
 
             services.AddControllers();
+
+            // Nastavljamo dalje sa konfiguracijom baze, sada je potrebno da odmah ispod services.AddControllers(); izvršimo konfiguraciju konteksta sa SQL bazom podataka
+            // Potrebno je da instaliramo i Microsoft.EntityFrameworkCore.SqlServer verziju 5.0.17 zbog .Net Core SDK verzije 5
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CandidateApp", Version = "v1" });
