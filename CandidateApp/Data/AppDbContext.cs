157a192b795c0b3367aef7f87a94087d132b408f
Filip Radosavljević <filip6331@gmail.com>
2022-08-02 12:57:59 +0200

    Kreirana model klasa kandidata Candidate.cs i kontekst klasa za komunikaciju sa bazom AppDbContext.cs

---
+++ CandidateApp/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CandidateApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateApp.Data
+{
+    // Nasledili smo klasu DbContext što znači da možemo koristiti njene metode i ostalo za komunikaciju sa bazom
+    public class AppDbContext : DbContext
+    {
+        // Kreiramo konstruktor i da bi uključio potrebne elemente u njega, treba da izgleda ovako:
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        // Sada vršimo referenciranje model klase sa tabelom u bazi podataka
+        // Promenjiva će biti tipa DbSet i nazvaćemo je kao tabelu u bazi Books. Sada možemo manipulisati tabelom uz pomoć ovog naziva "Books"
+        public DbSet<Candidate> Books { get; set; }
+    }
+}
---
+++ CandidateApp/Data/Models/Candidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateApp.Data.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public string EmailId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AdressLine1 { get; set; }
+        public string AdressLine2 { get; set; }
+        public bool Active { get; set; }
+    }
+}
