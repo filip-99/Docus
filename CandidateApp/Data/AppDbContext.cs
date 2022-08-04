b3103b97f17cf07b09c353b25a60de08ca5284f2
Filip Radosavljević <filip6331@gmail.com>
2022-08-04 12:53:16 +0200

    U kontekst klasi AppDbContext.cs kreirana veza između referentnih modela i definisani nazivi kojima će se upravljati tabelama

--- CandidateApp/Data/AppDbContext.cs
+++ CandidateApp/Data/AppDbContext.cs
@@ -16,8 +16,31 @@ namespace CandidateApp.Data
         {
 
         }
+
+        // Potrebno je sada da konfigurišemo ova 3 modela (Author.cs, Book.cs i Book_Author.cs) kako bi uspešno apdejtovali bazu
+        // Ovo se konfiguriše korišćenjem Fluent API-ja za Entity Framework Core
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Sada definišemo odnos između Candidate i FormAction
+            modelBuilder.Entity<FormAction>()
+                // HasOne - jedan kandidat
+                .HasOne(c => c.Candidate)
+                // WithMany - Može imati više FormAction (više je na strani FormAction)
+                .WithMany(fa => fa.FormAction)
+                // Sada definišemo javni ključ
+                .HasForeignKey(fa => fa.CandidateId);
+
+            // Sada isto ovako definišemo odnos između Form i FormAction
+            modelBuilder.Entity<FormAction>()
+                .HasOne(a => a.Form)
+                .WithMany(ba => ba.FormAction)
+                .HasForeignKey(ai => ai.FormId);
+        }
+
         // Sada vršimo referenciranje model klase sa tabelom u bazi podataka
         // Promenjiva će biti tipa DbSet i nazvaćemo je kao tabelu u bazi Books. Sada možemo manipulisati tabelom uz pomoć ovog naziva "Books"
         public DbSet<Candidate> Candidates { get; set; }
+        public DbSet<Form> Forms { get; set; }
+        public DbSet<FormAction> FormsActions { get; set; }
     }
 }
