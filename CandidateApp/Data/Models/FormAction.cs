717133aea7dbe4549d920aeabac9cb6af4f6740d
Filip Radosavljević <filip6331@gmail.com>
2022-08-04 12:04:27 +0200

    U klasi FormAction.cs definisana veza ka Candidate.cs i Form.cs

---
+++ CandidateApp/Data/Models/FormAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateApp.Data.Models
+{
+    public class FormAction
+    {
+        public int Id { get; set; }
+        public int CandidateId { get; set; }
+        public int FormId { get; set; }
+        public string Action { get; set; }
+        public DateTime ActionOn { get; set; }
+
+        // Definišemo odnos između tabela
+        public Candidate Candidate { get; set; }
+        public Form Form { get; set; }
+    }
+}
