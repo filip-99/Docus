c757e78b8fe579389b2630b7dd57ac6e516f3fe0
Filip Radosavljević <filip6331@gmail.com>
2022-08-04 12:05:41 +0200

    U klasi Form.cs definisana veza više ka tabeli FormAction.cs, dodavanjem liste tipa FormAction

---
+++ CandidateApp/Data/Models/Form.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateApp.Data.Models
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+
+        // Dodajemo odnos između klase FormAction
+        public List<FormAction> FormAction { get; set; }
+    }
+}
