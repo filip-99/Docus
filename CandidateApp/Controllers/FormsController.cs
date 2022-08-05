903f6e4dbe841bf6878057f8c78f44348fe785d4
Filip Radosavljević <filip6331@gmail.com>
2022-08-05 10:22:28 +0200

    Kreirana kontroler klasa FormsController.cs sa API End-Pointom za ubacivanje podataka u tabelu form

---
+++ CandidateApp/Controllers/FormsController.cs
@@ -0,0 +1,32 @@
+﻿using CandidateApp.Data.Services;
+using CandidateApp.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormsController : ControllerBase
+    {
+
+        private FormsService _formsService;
+
+        public FormsController(FormsService formsService)
+        {
+            _formsService = formsService;
+        }
+
+
+        [HttpPost("add-form")]
+        public IActionResult AddForm([FromBody] FormVM form)
+        {
+            _formsService.AddForm(form);
+            return Ok();
+        }
+    }
+}
