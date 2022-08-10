17da57c3bda5cb8ce1a87b66d378d334cb24f9e5
Filip Radosavljević <filip6331@gmail.com>
2022-08-10 13:02:37 +0200

    Prepravljena metoda za prikaz kandidata a osnovu ID-a kandidata u klasi ControllersCandidates.cs

--- CandidateApp/Controllers/CandidatesController.cs
+++ CandidateApp/Controllers/CandidatesController.cs
@@ -37,7 +37,7 @@ namespace CandidateApp.Controllers
         [HttpGet("get-candidate-by-id/{id}")]
         public IActionResult GetCandidateById(int id)
         {
-            var candidate = _candidatesService.GetCandidateById(id);
+            var candidate = _candidatesService.GetCandidateWithFormById(id);
             return Ok(candidate);
         }
 
@@ -52,12 +52,12 @@ namespace CandidateApp.Controllers
         // Sada će mo implementirati EndPoint tj. krajnje tačke
         // Prva će biti HttpPost jer šaljemo podatke u bazu
         // Da bi osigurali da se radi baš o ovoj krajnjoj tački dodelićemo naziv
-        [HttpPost("add-candidate")]
+        [HttpPost("add-candidate-with-forms-actions")]
         // Tip metode IActionResult || AddBook - naziv metode || [FromBody] uzima se kontekst odnosno telo klase BookVM
         // Prosleđeni parametar [FromBody]BookVM book znači da se očekuje pri unosu da se popune sva polja u klasi BookVM
         public IActionResult AddCandidate([FromBody] CandidateVM candidate)
         {
-            _candidatesService.AddBook(candidate);
+            _candidatesService.AddCandidateWithForms(candidate);
             // Ok() - ovo je samo jedan od tipova koje metoda može da vrati
             return Ok();
         }
